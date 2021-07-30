a70bdcc6aaf90ab9626d63336b51e66b27d7085b
vlastee <vlastee@mail.ua>
2021-07-30 02:42:46 -0400

    fix popup on avatar

--- src/features/TaskElement/ChildrenElements/CustomAvatar.js
+++ src/features/TaskElement/ChildrenElements/CustomAvatar.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Popover from '@material-ui/core/Popover';
-import Typography from '@material-ui/core/Typography';
-import { Avatar } from '@material-ui/core';
+import { Avatar, Typography, Popper, Paper } from '@material-ui/core';
 
 
 
@@ -14,31 +12,33 @@ export default function CustomAvatar(props) {
     const [anchorEl, setAnchorEl] = React.useState(null);
 
     const handleOnHover = (event) => {
+        console.log("Hovered");
         console.log(event);
-        setAnchorEl(event.currentTarget);
+        setAnchorEl(event.target);
     };
 
-    const handleOnLeave = () => {
+    const handleOnLeave = (event) => {
+        console.log("Left");
+        console.log(event);
         setAnchorEl(null);
     };
 
     const open = Boolean(anchorEl);
     const id = open ? 'simple-popover' : undefined;
 
-    const handleDeleteTag = () => {
-
-    }
 
     return (
         <>
             <Avatar
                 alt={props.user.user_first_name + " " + props.user.user_last_name}
                 src="."
+                aria-owns={open ? 'mouse-over-popover' : undefined}
+                aria-haspopup="true"
                 onMouseEnter={(e) => handleOnHover(e)}
-                onMouseLeave={() => handleOnLeave()}
+                onMouseLeave={(e) => handleOnLeave(e)}
             />
 
-            <Popover
+            <Popper
                 id={id}
                 open={open}
                 anchorEl={anchorEl}
@@ -51,8 +51,10 @@ export default function CustomAvatar(props) {
                     horizontal: 'center',
                 }}
             >
-                <Typography >{props.user.user_first_name + " " + props.user.user_last_name}</Typography>
-            </Popover>
+                <Paper>
+                    <Typography >{props.user.user_first_name + " " + props.user.user_last_name}</Typography>
+                </Paper>
+            </Popper>
         </>
     )
 }
